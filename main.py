2de56380a7de71e1e97c17c462821399a63e5bb7
Vasu24122006 <vasu34141@gmail.com>
2024-12-03 17:01:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import time
 import json
 import telebot
 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello from Koyeb!"
+
+if __name__ == "__main__":
+    # PORT environment variable se port fetch karte hain, agar nahi ho toh 5000 default hai
+    port = int(os.environ.get("PORT", 5000))  # Default 5000
+    app.run(host="0.0.0.0", port=port)
+  
+
 ##TOKEN DETAILS
 TOKEN = "TRON"
 
